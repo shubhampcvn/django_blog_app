6a5d236c6b04fe63a95e75b41bfc4e35be8daa54
shubhampcvn <shubhampcvn@gmail.com>
2020-10-19 14:57:04 +0530

    Updating Admin view screen for account app

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from account.models import Account
 
 
 def home_screen_view(request):
     context = {}
-    context['some_string']= 'sample string'
+    accounts = Account.objects.all()
+    context['accounts'] = accounts
     return render(request, "personal/home.html", context)
