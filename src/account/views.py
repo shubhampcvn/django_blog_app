ad1746bc6609d8fee16f3679c5d8181c449df40f
shubhampcvn <shubhampcvn@gmail.com>
2020-10-20 11:00:54 +0530

    Creating dedicated login page

--- src/account/views.py
+++ src/account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from .forms import RegistrationForm
+from .forms import RegistrationForm, AccountAuthenticationForm
 
 
 def registration_view(request):
@@ -25,4 +25,22 @@ def registration_view(request):
 def logout_view(request):
     logout(request)
     return redirect('home')
+
+
+def login_view(request):
+    context = {}
+    if request.POST:
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(email=email, password=raw_password)
+            if user:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AccountAuthenticationForm()
+
+    context['login_form'] = form
+    return render(request, 'account/login.html', context)
 
