e58ff71aa49b53b0c76af2c019140e6a047ce0ac
shubhampcvn <shubhampcvn@gmail.com>
2020-10-20 13:39:37 +0530

    Creating multiple header and footer snippets

--- src/account/forms.py
+++ src/account/forms.py
@@ -50,4 +50,4 @@ class AccountUpdateForm(forms.ModelForm):
                 account = Account.objects.exclude(pk=self.instance.pk).get(username=username)
             except Account.DoesNotExist:
                 return username
-            raise forms.ValidationError('Usernmae "%s" is already in use.'%username)
+            raise forms.ValidationError('Username "%s" is already in use.'%username)
