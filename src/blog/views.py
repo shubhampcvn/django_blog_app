6ecc6ab72f9331e5237a95a0610a75c21468c80c
shubhampcvn <shubhampcvn@gmail.com>
2020-10-22 20:02:04 +0530

    Adding blog edit functionality

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import CreateBlogPostForm
+from .forms import CreateBlogPostForm, UpdateBlogPostForm
 from account.models import Account
 from blog.models import BlogPost
 
@@ -32,3 +32,34 @@ def detail_blog_view(request, slug):
     context['blog_post'] = blog_post
 
     return render(request, 'blog/detail_blog.html', context)
+
+
+
+def edit_blog_view(request, slug):
+
+    context = {}
+
+    user = request.user
+
+    if not user.is_authenticated:
+        return redirect('must_authenticate')
+
+    blog_post = get_object_or_404(BlogPost, slug=slug)
+
+    if request.POST:
+        form = UpdateBlogPostForm(request.POST or None, request.FILES or None, instance=blog_post)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.save()
+            context['success_message'] = 'Updated'
+            blog_post = obj
+    form = UpdateBlogPostForm(
+        initial={
+            "title": blog_post.title,
+            "body": blog_post.body,
+            "image": blog_post.image,
+        }
+    )
+
+    context['form'] = form
+    return render(request, 'blog/edit_blog.html', context)
